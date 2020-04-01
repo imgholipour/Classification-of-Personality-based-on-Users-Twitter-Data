f7fbe139d4188f993e6adfda9b2405b560a981e4
priyansh19 <priyansh7533053411@gmail.com>
2020-04-01 18:22:53 +0530

    Cleaning Up the Bottleneck

--- pyGen.py
+++ pyGen.py
@@ -12,17 +12,18 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn import svm
 csvFile=open('CSV_Data/newfrequency300.csv', 'rt')
 csvReader=csv.reader(csvFile)
-print('Reading stop_words from database...')
+print('\n Reading stop_words from database...')
 mydict={row[1]: int(row[0]) for row in csvReader}
-pprint.pprint(mydict)
+#pprint.pprint(mydict)
 
-print('<===================================================================================>')
+print('===================================================================================> \n')
 y=[]
-print('Training Model for Judging/Perception ...')
+print('Training Model for Judging/Perception ... \n')
 with open ('CSV_Data/PJFinaltest.csv', 'rt') as f:
 	reader=csv.reader(f)
 	corpus=[rows[0] for rows in reader]
 
+print('Reading data from file PJFinaltest.csv')
 with open ('CSV_Data/PJFinaltest.csv', 'rt') as f:
 	csvReader1=csv.reader(f)
 	for rows in csvReader1:
@@ -36,21 +37,22 @@ train = X.sample(frac=0.8, random_state=1)
 test=X.drop(train.index)
 y_train=train[301]
 y_test=test[301]
-print(train.shape)
-print(test.shape)
+print('Size of training data ' + str(train.shape))
+print('Size of testing data ' + str(test.shape))
 xtrain=train.drop(301,axis=1)
 xtest=test.drop(301,axis=1)
 model.fit(xtrain,y_train)
 pickle.dump(model, open('Pickle_Data/BNPJFinal.sav', 'wb'))
 del result
 
-print('<===================================================================================>')
+print('===================================================================================> \n')
 y=[]
-print('Training Model for Introversion/Extraversion ...')
+print('Training Model for Introversion/Extraversion ... \n')
 with open ('CSV_Data/IEFinaltest.csv', 'rt') as f:
 	reader=csv.reader(f)
 	corpus=[rows[0] for rows in reader]
 
+print('Reading data from file IEFinaltest.csv')
 with open ('CSV_Data/IEFinaltest.csv', 'rt') as f:
 	csvReader1=csv.reader(f)
 	for rows in csvReader1:
@@ -64,21 +66,22 @@ train = X.sample(frac=0.8, random_state=1)
 test=X.drop(train.index)
 y_train=train[301]
 y_test=test[301]
-print(train.shape)
-print(test.shape)
+print('Size of training data ' + str(train.shape))
+print('Size of testing data ' + str(test.shape))
 xtrain=train.drop(301,axis=1)
 xtest=test.drop(301,axis=1)
 model.fit(xtrain,y_train)
 pickle.dump(model, open('Pickle_Data/BNIEFinal.sav', 'wb'))
 del result
 
-print('<===================================================================================>')
+print('===================================================================================> \n')
 y=[]
-print('Training Model for Thinking/Feeling ...')
+print('Training Model for Thinking/Feeling ... \n')
 with open ('CSV_Data/TFFinaltest.csv', 'rt') as f:
 	reader=csv.reader(f)
 	corpus=[rows[0] for rows in reader]
 
+print('Reading data from file TFFinaltest.csv')
 with open ('CSV_Data/TFFinaltest.csv', 'rt') as f:
 	csvReader1=csv.reader(f)
 	for rows in csvReader1:
@@ -92,21 +95,21 @@ train = X.sample(frac=0.8, random_state=1)
 test=X.drop(train.index)
 y_train=train[301]
 y_test=test[301]
-print(train.shape)
-print(test.shape)
+print('Size of training data ' + str(train.shape))
+print('Size of testing data ' + str(test.shape))
 xtrain=train.drop(301,axis=1)
 xtest=test.drop(301,axis=1)
 model.fit(xtrain,y_train)
 pickle.dump(model, open('Pickle_Data/BNTFFinal.sav', 'wb'))
 del result
 
-print('<===================================================================================>')
+print('===================================================================================> \n')
 y=[]
-print('Training Model for Sensing/Intuition ...')
+print('Training Model for Sensing/Intuition ... \n')
 with open ('CSV_Data/SNFinaltest.csv', 'rt') as f:
 	reader=csv.reader(f)
 	corpus=[rows[0] for rows in reader]
-
+print('Reading data from file SNFinaltest.csv')
 with open ('CSV_Data/SNFinaltest.csv', 'rt') as f:
 	csvReader1=csv.reader(f)
 	for rows in csvReader1:
@@ -120,9 +123,12 @@ train = X.sample(frac=0.8, random_state=1)
 test=X.drop(train.index)
 y_train=train[301]
 y_test=test[301]
-print(train.shape)
-print(test.shape)
+print('Size of training data ' + str(train.shape))
+print('Size of testing data ' + str(test.shape))
 xtrain=train.drop(301,axis=1)
 xtest=test.drop(301,axis=1)
 model.fit(xtrain,y_train)
 pickle.dump(model, open('Pickle_Data/BNSNFinal.sav', 'wb'))
+
+
+print('\nSaving all the 4 models trained into a pickle file. ')
